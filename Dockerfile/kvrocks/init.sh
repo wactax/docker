ed65f9aa2db5be64d1d1677ec6861b212a297f6d
wacdev <i@wac.tax>
2023-05-03 09:37:39 +0800

    ♨

--- Dockerfile/kvrocks/init.sh
+++ Dockerfile/kvrocks/init.sh
@@ -8,3 +8,6 @@ if [ ! -d "kvrocks" ]; then
   latest_version=$(curl -s https://api.github.com/repos/apache/incubator-kvrocks/releases/latest | grep tag_name | cut -d '"' -f 4)
   git clone -b $latest_version --depth=1 git@github.com:apache/incubator-kvrocks.git kvrocks
 fi
+
+cd kvrocks
+docker build -t kvrocks .
